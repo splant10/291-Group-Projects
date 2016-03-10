134ca0445089aee0e6f10328ae31046e0ff416d0
splant10 <splant.10@gmail.com>
2016-03-09 18:55:51 -0700

    changes to the GUI. Added tabs for each assignment 'question'

---
+++ Assign1/bin/AutoTransaction.java
@@ -0,0 +1,18 @@
+/* Note: Java has the auto commit feature. Thus, all changes made are final and can be seen in the DB instantly.
+ * javac CreateToffees.java
+ * java CreateToffees
+ * 
+ * Based off of Java/SQL tutorial from CMPUT 291 Lab by Kriti Khare
+ * 9 March 2016
+ * Spencer Plant
+ */
+
+package bin;
+
+import java.util.*;
+import java.sql.*; // Java package for accessing Oracle
+import java.io.*; // Java package includes Console for getting password from user and printing to screen
+
+public class AutoTransaction {
+	
+}
--- Assign1/bin/GroupProject1.java
+++ Assign1/bin/GroupProject1.java
@@ -3,10 +3,17 @@ package bin;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
 
 public class GroupProject1 {
 
 	private JFrame frame;
+	private JTextField textUserName;
+	private JPasswordField textPassword;
 
 	/**
 	 * Launch the application.
@@ -36,8 +43,44 @@ public class GroupProject1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 488, 441);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		textUserName = new JTextField();
+		textUserName.setBounds(12, 33, 221, 19);
+		frame.getContentPane().add(textUserName);
+		textUserName.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(12, 12, 100, 15);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(245, 12, 100, 15);
+		frame.getContentPane().add(lblPassword);
+		
+		textPassword = new JPasswordField();
+		textPassword.setBounds(245, 33, 229, 19);
+		frame.getContentPane().add(textPassword);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(12, 64, 462, 339);
+		frame.getContentPane().add(tabbedPane);
+		
+		JPanel tabLicenceRegistration = new JPanel();
+		tabbedPane.addTab("Driver Licence Registration", null, tabLicenceRegistration, null);
+		
+		JPanel tabViolationRecord = new JPanel();
+		tabbedPane.addTab("Violation Record", null, tabViolationRecord, null);
+		
+		JPanel tabSearch = new JPanel();
+		tabbedPane.addTab("Search", null, tabSearch, null);
+		
+		JPanel tabRegistration = new JPanel();
+		tabbedPane.addTab("Vehicle Registration", null, tabRegistration, null);
+		
+		JPanel tabTransaction = new JPanel();
+		tabbedPane.addTab("Auto Transaction", null, tabTransaction, null);
 	}
 }
