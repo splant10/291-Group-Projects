ce98fa2c3ce83746da1590e003074f15ac54015a
splant10 <splant.10@gmail.com>
2016-03-17 13:03:25 -0600

    added calendar picker to Vehicle Registration page.

---
+++ Assign1/bin/DateLabelFormatter.java
@@ -0,0 +1,34 @@
+// This class is from this
+// http://stackoverflow.com/questions/26794698/how-do-i-implement-jdatepicker
+// StackOverflow page, answered by MadProgrammer. 
+// This is a class to help get calendar functionality in our program.
+
+package bin;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.JFormattedTextField.AbstractFormatter;
+
+public class DateLabelFormatter extends AbstractFormatter {
+
+    private String datePattern = "yyyy-MM-dd";
+    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+    @Override
+    public Object stringToValue(String text) throws ParseException {
+        return dateFormatter.parseObject(text);
+    }
+
+    @Override
+    public String valueToString(Object value) throws ParseException {
+        if (value != null) {
+            Calendar cal = (Calendar) value;
+            return dateFormatter.format(cal.getTime());
+        }
+
+        return "";
+    }
+
+}
