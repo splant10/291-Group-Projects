bc4e7ccec77456d71358c0946141e996a3aaf035
splant10 <splant.10@gmail.com>
2016-03-17 12:37:21 -0600

    changes to Vehicle Registration layout

--- Assign1/bin/GroupProject1.java
+++ Assign1/bin/GroupProject1.java
@@ -15,10 +15,15 @@ import javax.swing.JPasswordField;
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.sql.*; // Java package for accessing Oracle
+import java.sql.*;
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerListModel;
+import javax.swing.JRadioButton; // Java package for accessing Oracle
 
 public class GroupProject1 {
 
@@ -37,6 +42,18 @@ public class GroupProject1 {
     private static String m_driverName = "oracle.jdbc.driver.OracleDriver";
     private static Connection m_con;
     private boolean loggedIn = false;
+    private JTextField textVRegistrationVID;
+    private JTextField textVRegistrationMake;
+    private JTextField textVRegistrationModel;
+    private JTextField textVRegistrationYear;
+    private JTextField textVRegistrationColor;
+    private JTextField textVRegistrationOwnerSIN;
+    private JTextField textVRegistrationOName;
+    private JTextField textVRegistrationOHeight;
+    private JTextField textField;
+    private JTextField textVRegistrationOEye;
+    private JTextField textVRegistrationOHair;
+    private JTextField textVRegistrationOAddress;
     
 	
 	/**
@@ -93,7 +110,7 @@ public class GroupProject1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 488, 441);
+		frame.setBounds(100, 100, 488, 488);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -117,8 +134,161 @@ public class GroupProject1 {
 		
 		// Make tabs for the different application programs
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(12, 64, 462, 339);
+		tabbedPane.setBounds(12, 64, 462, 386);
 		frame.getContentPane().add(tabbedPane);
+		
+		JPanel tabRegistration = new JPanel();
+		tabbedPane.addTab("Vehicle Registration", null, tabRegistration, null);
+		tabRegistration.setLayout(null);
+		
+		// Following is layout code for the Vehicle Registration Tab //
+			textVRegistrationVID = new JTextField();
+			textVRegistrationVID.setBounds(12, 28, 114, 19);
+			tabRegistration.add(textVRegistrationVID);
+			textVRegistrationVID.setColumns(10);
+			
+			JLabel lblVRegistrationVID = new JLabel("Vehicle Serial #");
+			lblVRegistrationVID.setBounds(12, 12, 114, 15);
+			tabRegistration.add(lblVRegistrationVID);
+			
+			textVRegistrationMake = new JTextField();
+			textVRegistrationMake.setBounds(158, 28, 114, 19);
+			tabRegistration.add(textVRegistrationMake);
+			textVRegistrationMake.setColumns(10);
+			
+			JLabel lblVRegistrationMake = new JLabel("Make");
+			lblVRegistrationMake.setBounds(158, 12, 70, 15);
+			tabRegistration.add(lblVRegistrationMake);
+			
+			textVRegistrationModel = new JTextField();
+			textVRegistrationModel.setBounds(302, 28, 114, 19);
+			tabRegistration.add(textVRegistrationModel);
+			textVRegistrationModel.setColumns(10);
+			
+			JLabel lblVRegistrationModel = new JLabel("Model");
+			lblVRegistrationModel.setBounds(302, 12, 70, 15);
+			tabRegistration.add(lblVRegistrationModel);
+			
+			textVRegistrationYear = new JTextField();
+			textVRegistrationYear.setBounds(12, 73, 114, 19);
+			tabRegistration.add(textVRegistrationYear);
+			textVRegistrationYear.setColumns(10);
+			
+			JLabel lblVRegistrationYear = new JLabel("Year");
+			lblVRegistrationYear.setBounds(12, 59, 70, 15);
+			tabRegistration.add(lblVRegistrationYear);
+			
+			textVRegistrationColor = new JTextField();
+			textVRegistrationColor.setBounds(158, 73, 114, 19);
+			tabRegistration.add(textVRegistrationColor);
+			textVRegistrationColor.setColumns(10);
+			
+			JLabel lblVRegistrationColor = new JLabel("Color");
+			lblVRegistrationColor.setBounds(158, 59, 70, 15);
+			tabRegistration.add(lblVRegistrationColor);
+			
+			JLabel lblVRegistrationTypeID = new JLabel("Type");
+			lblVRegistrationTypeID.setBounds(302, 59, 70, 15);
+			tabRegistration.add(lblVRegistrationTypeID);
+			
+			JSpinner spinVRegistrationTypeID = new JSpinner();
+			spinVRegistrationTypeID.setModel(new SpinnerListModel(new String[] {"1, Sedan", "2, Coupe", "3, Sport", "4, Hatchback", "5, Truck", "6, Mini Van", "7, Semi", "8, SUV"}));
+			spinVRegistrationTypeID.setBounds(302, 73, 114, 20);
+			tabRegistration.add(spinVRegistrationTypeID);
+			
+			textVRegistrationOwnerSIN = new JTextField();
+			textVRegistrationOwnerSIN.setBounds(12, 147, 114, 19);
+			tabRegistration.add(textVRegistrationOwnerSIN);
+			textVRegistrationOwnerSIN.setColumns(10);
+			
+			JLabel lblVRegistrationOwnerSIN = new JLabel("SIN");
+			lblVRegistrationOwnerSIN.setBounds(12, 130, 114, 15);
+			tabRegistration.add(lblVRegistrationOwnerSIN);
+			
+			textVRegistrationOName = new JTextField();
+			textVRegistrationOName.setEditable(false);
+			textVRegistrationOName.setBounds(158, 147, 114, 19);
+			tabRegistration.add(textVRegistrationOName);
+			textVRegistrationOName.setColumns(10);
+			
+			JLabel lblOwnerName = new JLabel("Name");
+			lblOwnerName.setBounds(158, 130, 114, 15);
+			tabRegistration.add(lblOwnerName);
+			
+			textVRegistrationOHeight = new JTextField();
+			textVRegistrationOHeight.setEditable(false);
+			textVRegistrationOHeight.setBounds(302, 147, 114, 19);
+			tabRegistration.add(textVRegistrationOHeight);
+			textVRegistrationOHeight.setColumns(10);
+			
+			JLabel lblVRegistrationOHeight = new JLabel("Height");
+			lblVRegistrationOHeight.setBounds(302, 130, 70, 15);
+			tabRegistration.add(lblVRegistrationOHeight);
+			
+			JLabel lblNewLabel = new JLabel("Owner Information:");
+			lblNewLabel.setBounds(12, 104, 233, 15);
+			tabRegistration.add(lblNewLabel);
+			
+			textField = new JTextField();
+			textField.setEditable(false);
+			textField.setBounds(12, 194, 114, 19);
+			tabRegistration.add(textField);
+			textField.setColumns(10);
+			
+			JLabel lblVRegistrationOWeight = new JLabel("Weight");
+			lblVRegistrationOWeight.setBounds(12, 178, 70, 15);
+			tabRegistration.add(lblVRegistrationOWeight);
+			
+			textVRegistrationOEye = new JTextField();
+			textVRegistrationOEye.setEditable(false);
+			textVRegistrationOEye.setBounds(158, 194, 114, 19);
+			tabRegistration.add(textVRegistrationOEye);
+			textVRegistrationOEye.setColumns(10);
+			
+			JLabel lblVRegistrationOEye = new JLabel("Eye Color");
+			lblVRegistrationOEye.setBounds(158, 178, 70, 15);
+			tabRegistration.add(lblVRegistrationOEye);
+			
+			textVRegistrationOHair = new JTextField();
+			textVRegistrationOHair.setEditable(false);
+			textVRegistrationOHair.setBounds(302, 194, 114, 19);
+			tabRegistration.add(textVRegistrationOHair);
+			textVRegistrationOHair.setColumns(10);
+			
+			JLabel lblVRegistrationOHair = new JLabel("Hair Color");
+			lblVRegistrationOHair.setBounds(302, 178, 92, 15);
+			tabRegistration.add(lblVRegistrationOHair);
+			
+			textVRegistrationOAddress = new JTextField();
+			textVRegistrationOAddress.setEditable(false);
+			textVRegistrationOAddress.setBounds(12, 241, 114, 19);
+			tabRegistration.add(textVRegistrationOAddress);
+			textVRegistrationOAddress.setColumns(10);
+			
+			JLabel lblVRegistrationOAddress = new JLabel("Address");
+			lblVRegistrationOAddress.setBounds(12, 225, 70, 15);
+			tabRegistration.add(lblVRegistrationOAddress);
+			
+			JLabel lblVRegistrationOGender = new JLabel("Gender");
+			lblVRegistrationOGender.setBounds(158, 225, 70, 15);
+			tabRegistration.add(lblVRegistrationOGender);
+			
+			JRadioButton rdbtnVRegistrationMale = new JRadioButton("M");
+			rdbtnVRegistrationMale.setEnabled(false);
+			rdbtnVRegistrationMale.setBounds(158, 239, 56, 23);
+			tabRegistration.add(rdbtnVRegistrationMale);
+			
+			JRadioButton rdbtnVRegistrationFemale = new JRadioButton("F");
+			rdbtnVRegistrationFemale.setEnabled(false);
+			rdbtnVRegistrationFemale.setBounds(223, 239, 149, 23);
+			tabRegistration.add(rdbtnVRegistrationFemale);
+			
+			ButtonGroup genderGroup = new ButtonGroup();
+			genderGroup.add(rdbtnVRegistrationMale);
+			genderGroup.add(rdbtnVRegistrationFemale);
+			
+
+		// End layout code for the Vehicle Registration Tab //
 		
 		JPanel tabLicenceRegistration = new JPanel();
 		tabbedPane.addTab("Driver Licence Registration", null, tabLicenceRegistration, null);
@@ -129,13 +299,10 @@ public class GroupProject1 {
 		JPanel tabSearch = new JPanel();
 		tabbedPane.addTab("Search", null, tabSearch, null);
 		
-		JPanel tabRegistration = new JPanel();
-		tabbedPane.addTab("Vehicle Registration", null, tabRegistration, null);
-		
 		JPanel tabTransaction = new JPanel();
 		tabbedPane.addTab("Auto Transaction", null, tabTransaction, null);
 		tabTransaction.setLayout(null);
-			//
+			
 			textTransactionBuyer = new JTextField();
 			textTransactionBuyer.setBounds(12, 27, 209, 19);
 			tabTransaction.add(textTransactionBuyer);
