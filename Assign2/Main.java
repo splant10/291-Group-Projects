77dd1478edb112a61ee5c991bcf1caa55b1fe769
splant10 <splant.10@gmail.com>
2016-04-02 15:19:03 -0600

    changed indent format to entirely tabs

--- Assign2/Main.java
+++ Assign2/Main.java
@@ -26,67 +26,67 @@ public class Main{
 	private static final int DESTROY_DATABASE_OPTION = 5;
 	private static final int QUIT_OPTION = 6;
 
-    /*
-     *  the main function
-     */
-    public static void main(String[] args) {
-    	
-        while(true) {
-            //TO-DO: read the db_type_option from command line
-        	String db_type_option = "1";
+	/*
+	 *  the main function
+	 */
+	public static void main(String[] args) {
+		
+		while(true) {
+			//TO-DO: read the db_type_option from command line
+			String db_type_option = "1";
 
-        	MyDatabase myDatabase = new MyDatabase(db_type_option);
+			MyDatabase myDatabase = new MyDatabase(db_type_option);
 
-        	Scanner in = new Scanner(System.in);
-        	
-        	printMenu();
-        
-        	String option = in.nextLine();
-        	
-        	switch(Integer.valueOf(option)){
+			Scanner in = new Scanner(System.in);
+			
+			printMenu();
+		
+			String option = in.nextLine();
+			
+			switch(Integer.valueOf(option)){
 
-        	case CREATE_POPULATE_DATABASE_OPTION:
-        		myDatabase.create();
-        		break;
-        	case RETRIEVE_RECORD_WITH_GIVEN_KEY_OPTION:
-        		System.out.println("Please enter the key and press enter");
-        		String key = in.nextLine();
-        		String value = myDatabase.getValue(key);
-        		System.out.println(value);
-        		break;
-        	case RETRIEVE_RECORD_WITH_GIVEN_DATA_OPTION:
-        		System.out.println("Please enter the value and press enter");
-        		String value1 = in.nextLine();
-        		String key1 = myDatabase.getKey(value1);
-        		System.out.println(key1);
-        		break;
-        	case RETRIEVE_RECORDS_WITH_GIVEN_RANGE_OF_KEY_OPTION:
-        		System.out.println("Please enter a list of keys to retrieve seperated by space");
-        		String keys[] = in.nextLine().split(" ");
-        		String values[] = new String[keys.length];
-        		
-                for (int i = 0; i < keys.length; i++){
-            		values[i] = myDatabase.getKey(keys[i]);
-            		System.out.println("key: " +  keys[i] + " value: " + values[i]);
-        		}
+			case CREATE_POPULATE_DATABASE_OPTION:
+				myDatabase.create();
+				break;
+			case RETRIEVE_RECORD_WITH_GIVEN_KEY_OPTION:
+				System.out.println("Please enter the key and press enter");
+				String key = in.nextLine();
+				String value = myDatabase.getValue(key);
+				System.out.println(value);
+				break;
+			case RETRIEVE_RECORD_WITH_GIVEN_DATA_OPTION:
+				System.out.println("Please enter the value and press enter");
+				String value1 = in.nextLine();
+				String key1 = myDatabase.getKey(value1);
+				System.out.println(key1);
+				break;
+			case RETRIEVE_RECORDS_WITH_GIVEN_RANGE_OF_KEY_OPTION:
+				System.out.println("Please enter a list of keys to retrieve seperated by space");
+				String keys[] = in.nextLine().split(" ");
+				String values[] = new String[keys.length];
+				
+				for (int i = 0; i < keys.length; i++){
+					values[i] = myDatabase.getKey(keys[i]);
+					System.out.println("key: " +  keys[i] + " value: " + values[i]);
+				}
 
-        		break;
-        	case DESTROY_DATABASE_OPTION:
-        		break;
-        	case QUIT_OPTION:
-                System.exit(1);
-        		break;
-        	}
-        }
-    }
+				break;
+			case DESTROY_DATABASE_OPTION:
+				break;
+			case QUIT_OPTION:
+				System.exit(1);
+				break;
+			}
+		}
+	}
 
-    private static void printMenu() {
-    	System.out.println("1 Create and populate a database\n"
-    			+ "2 Retrieve records with a given key\n"
-    			+ "3 Retrieve records with a given data\n"
-    			+ "4. Retrieve records with a given range of key values\n"
-    			+ "5. Destroy the database\n"
-    			+ "6. Quit");
+	private static void printMenu() {
+		System.out.println("\n1 Create and populate a database\n"
+				+ "2 Retrieve records with a given key\n"
+				+ "3 Retrieve records with a given data\n"
+				+ "4. Retrieve records with a given range of key values\n"
+				+ "5. Destroy the database\n"
+				+ "6. Quit\n");
 		
 	}
 }
--- Assign2/MyDatabase.java
+++ Assign2/MyDatabase.java
@@ -5,9 +5,9 @@ import com.sleepycat.db.*;
 
 public class MyDatabase {
 	// to specify the file name for the table
-    private static final String SAMPLE_TABLE = "tmp/ahmirza_db/sample_table";
-    // number of records to populate
-    private static final int NO_RECORDS = 1000;
+	private static final String SAMPLE_TABLE = "tmp/ahmirza_db/sample_table";
+	// number of records to populate
+	private static final int NO_RECORDS = 1000;
 	
 	private DatabaseConfig dbConfig;
 	private Database my_table;
@@ -18,20 +18,20 @@ public class MyDatabase {
 	}
 	
 	private void setDatabaseType(String db_type_option) {
-    	if (db_type_option == null || Integer.valueOf(db_type_option) > 0 && Integer.valueOf(db_type_option) < 4) {
-    		System.out.println("Please enter correct number of argument");
-    	}
+		if (db_type_option == null || Integer.valueOf(db_type_option) > 0 && Integer.valueOf(db_type_option) < 4) {
+			System.out.println("Please enter correct number of argument");
+		}
 		
 		switch (Integer.valueOf(db_type_option)){
-    	case 1:
-    		dbConfig.setType(DatabaseType.BTREE);
-    		break;
-    	case 2:
-    		dbConfig.setType(DatabaseType.HASH);
-    		break;
-    	case 3:
-    		break;
-    	}
+		case 1:
+			dbConfig.setType(DatabaseType.BTREE);
+			break;
+		case 2:
+			dbConfig.setType(DatabaseType.HASH);
+			break;
+		case 3:
+			break;
+		}
 	}
 
 	public void create() {
@@ -40,8 +40,8 @@ public class MyDatabase {
 			my_table = new Database(SAMPLE_TABLE, null, dbConfig);
 			System.out.println(SAMPLE_TABLE + " has been created");
 			/* populate the new database with NO_RECORDS records */
-    	    populateTable(my_table,NO_RECORDS);
-    	    System.out.println("1000 records inserted into" + SAMPLE_TABLE);
+			populateTable(my_table,NO_RECORDS);
+			System.out.println("1000 records inserted into" + SAMPLE_TABLE);
 		} catch (FileNotFoundException | DatabaseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,23 +50,23 @@ public class MyDatabase {
 	}
 	
 	/*
-     *  To pouplate the given table with nrecs records
-     */
-    static void populateTable(Database my_table, int nrecs ) {
-	int range;
-        DatabaseEntry kdbt, ddbt;
-	String s;
-
-	/*  
-	 *  generate a random string with the length between 64 and 127,
-	 *  inclusive.
-	 *
-	 *  Seed the random number once and once only.
+	 *  To pouplate the given table with nrecs records
 	 */
-	Random random = new Random(1000000);
+	static void populateTable(Database my_table, int nrecs ) {
+		int range;
+			DatabaseEntry kdbt, ddbt;
+		String s;
 
-        try {
-            for (int i = 0; i < nrecs; i++) {
+		/*  
+		 *  generate a random string with the length between 64 and 127,
+		 *  inclusive.
+		 *
+		 *  Seed the random number once and once only.
+		 */
+		Random random = new Random(1000000);
+
+		try {
+			for (int i = 0; i < nrecs; i++) {
 
 		/* to generate a key string */
 		range = 64 + random.nextInt( 64 );
@@ -78,33 +78,33 @@ public class MyDatabase {
 		kdbt = new DatabaseEntry(s.getBytes());
 		kdbt.setSize(s.length()); 
 
-                // to print out the key/data pair
-                System.out.println(s+"\n");	
+				// to print out the key/data pair
+				System.out.println(s+"\n");	
 
 		/* to generate a data string */
 		range = 64 + random.nextInt( 64 );
 		s = "";
 		for ( int j = 0; j < range; j++ ) 
 		  s+=(new Character((char)(97+random.nextInt(26)))).toString();
-                // to print out the key/data pair
-                // System.out.println(s);	
-                // System.out.println("");
+				// to print out the key/data pair
+				// System.out.println(s);	
+				// System.out.println("");
 		
 		/* to create a DBT for data */
 		ddbt = new DatabaseEntry(s.getBytes());
 		ddbt.setSize(s.length()); 
 
 		/* to insert the key/data pair into the database */
-                my_table.putNoOverwrite(null, kdbt, ddbt);
-            }
-        }
-        catch (DatabaseException dbe) {
-            System.err.println("Populate the table: "+dbe.toString());
-            System.exit(1);
-        }
-    }
+				my_table.putNoOverwrite(null, kdbt, ddbt);
+			}
+		}
+		catch (DatabaseException dbe) {
+			System.err.println("Populate the table: "+dbe.toString());
+			System.exit(1);
+		}
+	}
 
-    // Returns the value for the specified key
+	// Returns the value for the specified key
 	public String getValue(String key) {
 		/* to create a DBT for key */
 		DatabaseEntry kdbt = new DatabaseEntry();
